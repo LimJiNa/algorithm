4775572bb68c188798fc2f75ca62a060e129cd0e
LimJiNa <jn4624@gmail.com>
2022-07-13 00:14:43 +0900

    2022-07-13 commit

---
+++ solved/src/bronze05/P14652.java
@@ -0,0 +1,38 @@
+package bronze05;
+
+import java.util.Scanner;
+
+public class P14652 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String str = scan.nextLine();
+
+		System.out.println(solution(str));
+
+		scan.close();
+	}
+
+	public static String solution(String str) {
+		StringBuilder sb = new StringBuilder();
+
+		String[] arr = str.split(" ");
+		int n = Integer.parseInt(arr[0]);
+		int m = Integer.parseInt(arr[1]);
+		int x = Integer.parseInt(arr[2]);
+
+		int cnt = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (x == cnt++) {
+					sb.append(i).append(" ");
+					sb.append(j);
+					return sb.toString();
+				}
+			}
+		}
+
+		return sb.toString();
+	}
+}
---
+++ solved/src/bronze05/P14681.java
@@ -0,0 +1,32 @@
+package bronze05;
+
+import java.util.Scanner;
+
+public class P14681 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+
+		System.out.println(solution(x, y));
+
+		scan.close();
+	}
+
+	public static int solution(int x, int y) {
+		int answer = 0;
+
+		if (x > 0 && y > 0) {
+			answer = 1;
+		} else if (x > 0 && y < 0) {
+			answer = 4;
+		} else if (x < 0 && y < 0) {
+			answer = 3;
+		} else {
+			answer = 2;
+		}
+
+		return answer;
+	}
+}
